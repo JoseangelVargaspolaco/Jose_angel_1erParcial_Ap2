1c05541537cef187616c26e51964447fe797a4b5
JoseangelVargaspolaco <jose_vargas7@ucne.edu.do>
2023-02-08 21:55:37 -0400

    Ultimo Commit (Diseño de la interfaz de ususario)

--- app/src/main/java/com/ucne/proj_1erparcial_ap2/ui/Prestamo/PrestamoScreen.kt
+++ app/src/main/java/com/ucne/proj_1erparcial_ap2/ui/Prestamo/PrestamoScreen.kt
@@ -1,31 +1,41 @@
 package com.ucne.proj_1erparcial_ap2.ui.Prestamo
 
-import androidx.compose.runtime.Composable
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Save
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.ucne.proj_1erparcial_ap2.R
 import com.ucne.proj_1erparcial_ap2.data.local.entity.PrestamoEntity
 
 @Composable
 fun PrestamoScreen(viewModel: PrestamoViewModel = hiltViewModel()) {
 
-    Column(Modifier.fillMaxSize()) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
         PrestamoBody(viewModel)
 
         Spacer(modifier = Modifier.padding(14.dp))
+        Text(
+            text = "Lista de Prestamos", fontSize = 50.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
         val uiState by viewModel.uiState.collectAsState()
         PrestamoListScreen(uiState.prestamoList)
     }
@@ -40,10 +50,19 @@ private fun PrestamoBody(
 
         OutlinedTextField(
             modifier = Modifier
-                .padding(8.dp)
+                .padding(2.dp)
                 .fillMaxWidth(),
             value = viewModel.Deudor,
-            onValueChange = {  viewModel.Deudor = it },
+            onValueChange = { viewModel.Deudor = it },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(33.dp)
+                        .padding(4.dp)
+                )
+            },
             label = { Text("Deudor") }
         )
 
@@ -53,6 +72,15 @@ private fun PrestamoBody(
                 .fillMaxWidth(),
             value = viewModel.Concepto,
             onValueChange = { viewModel.Concepto = it },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.DocumentScanner,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(33.dp)
+                        .padding(4.dp)
+                )
+            },
             label = { Text("Concepto") }
         )
 
@@ -63,19 +91,32 @@ private fun PrestamoBody(
             value = viewModel.Monto,
             onValueChange = { viewModel.Monto = it },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.AttachMoney,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(33.dp)
+                        .padding(4.dp)
+                )
+            },
             label = { Text("Monto") }
         )
 
+        Spacer(modifier = Modifier.padding(10.dp))
+
         ExtendedFloatingActionButton(
             modifier = Modifier
-                .padding(8.dp)
-                .align(Alignment.End),
+                .size(60.dp,60.dp)
+                .align(Alignment.End)
+                .wrapContentSize(Alignment.Center),
             text = { Text("") },
             icon = { Icon(imageVector = Icons.Filled.Save, contentDescription = "Save") },
             onClick = { viewModel.insertar() }
         )
     }
 }
+
 @Composable
 private fun PrestamoListScreen(prestamoList: List<PrestamoEntity>) {
     LazyColumn {
@@ -89,26 +130,34 @@ private fun PrestamoRow(prestamo: PrestamoEntity) {
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .wrapContentSize(Alignment.Center)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
         ) {
+
             Text(
-                text = prestamo.Deudor,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.weight(3f)
+                text = prestamo.Deudor, fontSize = 50.sp,
+                style = MaterialTheme.typography.titleLarge
             )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        ) {
             Text(
                 text = prestamo.Concepto,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.weight(3f)
+                style = MaterialTheme.typography.titleLarge
             )
             Text(
                 String.format("%.2f", prestamo.Monto),
+                fontSize = 50.sp,
                 textAlign = TextAlign.End,
-                modifier = Modifier.weight(2f)
+                modifier = Modifier
+                    .weight(3f)
+                    .fillMaxWidth()
             )
         }
         Divider(Modifier.fillMaxWidth())
